4f3f46fcf071c8177047b37c3603aedd414fad80
Jonathon Hewitt <jhewittjrs@gmail.com>
2023-10-29 19:23:25 -0700

    fix issue with treesitter default parsers, use cattpuccin-latte

--- lua/jonathon/plugins.lua
+++ lua/jonathon/plugins.lua
@@ -77,5 +77,6 @@ return require('lazy').setup({
             { 'rafamadriz/friendly-snippets' },
         }
     },
-    {'akinsho/git-conflict.nvim', version = "*", config = true}
+    {'akinsho/git-conflict.nvim', version = "*", config = true},
+	{ "catppuccin/nvim", name = "catppuccin"}
 })
